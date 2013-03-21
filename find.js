d112f7dcc291d43864d1c43381828c999683951b
Alan Gutierrez <alan@prettyrobots.com>
2013-03-21 18:21:00 -0400

    Parameterize the `find.js` file suffix.
    Add a parameter to specify a `find.js` file suffix. Also, fixed descent
    into sub-directories which wasn't tested and wasn't working.
    
    Added a test for the `find.js` function.
    
    Closes #15.

--- find.js
+++ find.js
@@ -1,35 +1,45 @@
 var fs = require("fs"), path = require("path");
 
-function routes (base) {
-  var routes = [];
+function routes (base, suffix) {
+  var routes = [], dotted = '.' + suffix;
   function children (base, parts) {
     var dir = path.join.apply(path, [ base ].concat(parts)), files = [];
     fs.readdirSync(dir).forEach(function (entry) {
       var file = path.join(dir, entry), stat = fs.statSync(file);
       if (stat.isDirectory()) {
-        children(base, parts.concat(file));
+        children(base, parts.concat(entry));
       } else {
         files.push(entry);
       }
     });
     files.forEach(function (entry) {
-      var file = path.join(dir, entry);
-      if (/^[^_].*\.stencil$/.test(entry)) {
-        var $ = /^(.*?)(_?)\.stencil$/.exec(entry),
-            name = $[1], pathInfo = !! $[2],
-            route = parts.slice(0);
-        if (name != "index") route.push(name);
-        // Note that we do not use the file systems path separator when
-        // resolving stencils.
-        routes.push({
-          route: "/" + route.join("/"),
-          stencil: parts.concat(entry).join("/")
-        });
-        if (pathInfo) {
+      var file = path.join(dir, entry), suffixed;
+      if (entry.lastIndexOf(dotted) == entry.length - dotted.length) {
+        if (entry[0] != '_') {
+          var $ = /^(.*?)(_?)\.(.*)$/.exec(entry),
+              name = $[1], pathInfo = !! $[2], extension = $[3],
+              route = parts.slice(0);
+          if (name != "index") route.push(name);
+          // Note that we do not use the file systems path separator when
+          // resolving stencils.
           routes.push({
-            route: "/" + route.join("/") + "/**:pathInfo",
-            stencil: parts.concat(entry).join("/")
+            route: "/" + route.join("/"),
+            script: parts.concat(entry).join("/"),
+            path: route.slice(),
+            file: entry,
+            name: name,
+            extension: extension.slice(0, - dotted.length)
           });
+          if (pathInfo) {
+            routes.push({
+              route: "/" + route.join("/") + "/**:pathInfo",
+              script: parts.concat(entry).join("/"),
+              path: route.slice(),
+              file: entry,
+              name: name,
+              extension: extension.slice(0, - dotted.length)
+            });
+          }
         }
       }
     });
@@ -38,5 +48,5 @@ function routes (base) {
   return routes;
 }
 
-if (require.main === module) console.log(routes('stencils'));
+if (require.main === module) console.log(routes('t/register/fixtures', 'js'));
 else module.exports = routes;
